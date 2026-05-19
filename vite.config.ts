b46b697dc9317302557c914a6a67fa4aab7eebb9
Jean Kevin <jkevinbr@gmail.com>
2026-05-19 17:29:16 -0300

    fix

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 
 export default defineConfig({
-  base: process.env.GITHUB_PAGES === "true" ? "/Zitec.Ai/" : "/",
+  base: '/',
   server: {
     host: "::",
     port: 8081,
